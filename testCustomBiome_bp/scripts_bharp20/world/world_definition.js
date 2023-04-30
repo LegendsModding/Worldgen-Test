4ad4f1a67fa492a866ee046c657f8b24348260bf
DFMods <fr33dum@protonmail.com>
2023-04-30 17:46:10 -0400

    Barebones

--- testCustomBiome_bp/scripts_bharp20/world/world_definition.js
+++ testCustomBiome_bp/scripts_bharp20/world/world_definition.js
@@ -1,3 +1,12 @@
-SNIPPET_InheritsFromGameMode("frostfire", () => {
-    SetWorldGenDefinition(frostfireWorldGenDefinition)
+const biomeTestWorldGenDefinition = {
+    base: null,
+    modify: (filterManager) => {
+        filterManager.AppendFilter(["biomeTest", "center"], 1)
+        filterManager.AppendFilter(["biomeTest", "set1"], 1)
+        filterManager.AppendFilter(["biomeTest", "set2"], 1)
+    }
+}
+
+SNIPPET_InheritsFromGameMode("biomeTest", () => {
+    SetWorldGenDefinition(biomeTestWorldGenDefinition)
 })
